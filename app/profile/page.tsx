02f873bf1ef5c5539af090cde4a4408fd0e1d71b
Simard <floogles71@gmail.com>
2024-08-07 18:07:56 +0200

    feat(profile): created profile card, wip CV

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,43 @@
+"use client";
 import Image from "next/image";
+import './page.css';
+import { MutableRefObject, useRef } from "react";
+import CV from "./components/cv";
 
 export default function ProfilePage() {
-    return <main className="flex justify-center min-h-screen">
-        <Image src="/shrek.png" height="500" width="500" alt=":)"></Image>
-        <div>
-            <h1>This is me !</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro perferendis non suscipit modi quos necessitatibus neque quis, dolorum rerum, cumque, facere ducimus dicta iste cum veniam! Officiis animi quia ad.</p>
-        </div>
-    </main>
+
+    const CVRef = useRef<HTMLElement | null>(null);
+
+    const scrollToCV = () => {
+        CVRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return (
+        <main className="flex flex-col min-h-screen items-center">
+            <div className="h-screen p-6 flex items-center">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>Mon profil</h1>
+                    </div>
+                    <div className="card-content space-x-4">
+                        <Image src="/shrek.png" height="500" width="500" alt=":)" className="aspect-square w-48 h-48 rounded profile-picture"></Image>
+                        <div className="space-y-4">
+                            <p>Je suis Simon, <b>développeur web fullstack</b> (presque !) diplômé d'un BUT en informatique après trois ans de formation.</p>
+                            <p>Solitaire de nature, j’ai toujours passé beaucoup de temps libre sur un ordinateur et me suis lancé dans le développement en 2021.</p>
+                            <p>En dehors du code, j’adore m’immerger dans des <b>jeux vidéo</b> variés, explorer notre univers à travers <b>l’astronomie et la physique</b>, et comprendre notre monde grâce aux récits épiques de notre <b>histoire</b>.</p>
+                            <p>J’apprécie aussi <b>philosopher avec mes amis</b>, en discutant et débattant pendant de longues heures de sujets variés. Notre question du moment est de savoir si l’Homme dispose d’un réel libre arbitre et si il arrive à s'abstraire du déterminisme de l’univers. En grand optimiste, je defends corps et âme le oui !</p>
+                            <p>Explorez ce site pour en apprendre plus sur ma pratique du développement à travers mes projets et sur ma formation en découvrant mes compétences.</p>
+                            <p>Sinon, restez sur cette page pour voir mon CV numérique et mes expériences professionnelles.</p>
+                        </div>
+                    </div>
+                    <div className="card-footer">
+                        <button onClick={scrollToCV}>Voir mon CV</button>
+                    </div>
+                </div>
+            </div>
+            <div ref={CVRef as MutableRefObject<HTMLDivElement>} className="min-h-screen p-6">
+                <CV></CV>
+            </div>
+        </main>
+    );
 }
